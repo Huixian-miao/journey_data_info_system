d8e17c0c855a3bd2e9c002ab9e42236b0cdc217f
Huixian-miao <2548673744@qq.com>
2025-08-17 11:31:35 +0800

    增加jsp代码v4，可以访问http://localhost:8050/person/journeyInfo但是乱码

--- src/main/java/org/journey/Main.java
+++ src/main/java/org/journey/Main.java
@@ -3,13 +3,19 @@ package org.journey;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("org.journey.dao")
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
 
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
     }
 }
